e339dc872a2c453143a7d8499f42fe60d95eccc9
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-06-29 10:09:41 -0400

    forecast

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ var searchBtn = $("#searchBtn");
 searchBtn.click(currentWeather);
 function currentWeather() {
   var city = $("#city").val();
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=61f00a845310c4a4251fd6bfc588f89b"
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=61f00a845310c4a4251fd6bfc588f89b"
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
     console.log(response);
-    var date = moment().format("MMMM Do, YYYY");
+    var date = moment().format("M/D/YYYY");
     $(".date").text(date);
     var cityName = response.name;
     $(".city").text(cityName) 
     var iconCode = response.weather[0].icon
     var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
     $(".icon").attr("src", iconURL);
-    var temperature = (((response.main.temp - 273.15)*9)/5) + 32;
+    var temperature = response.main.temp
     $(".temperature").text("Temperature: " + temperature + "°F");
     var humidity = response.main.humidity;
     $(".humidity").text("Humidity: " + humidity + "%");
@@ -24,3 +24,47 @@ function currentWeather() {
     $(".windSpeed").text("Wind Speed: " + windSpeed + "MPH");
   })
 };
+
+searchBtn.click(forecast);
+function forecast (){
+    var forecastCity = $("#city").val();
+    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+forecastCity+"&units=imperial&appid=61f00a845310c4a4251fd6bfc588f89b"
+    $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var dateOne = moment().add(1,"days").format("M/D/YYYY");
+        $(".dateOne").text(dateOne)
+        var dateTwo = moment().add(2,"days").format("M/D/YYYY");
+        $(".dateTwo").text(dateTwo)
+        var dateThree = moment().add(3,"days").format("M/D/YYYY");
+        $(".dateThree").text(dateThree)
+        var dateFour = moment().add(4,"days").format("M/D/YYYY");
+        $(".dateFour").text(dateFour)
+        var dateFive = moment().add(5,"days").format("M/D/YYYY");
+        $(".dateFive").text(dateFive)
+
+        var temperatureOne = response.list[0].main.temp
+        $(".temperatureOne").text("Temp: " + temperatureOne +"°F");
+        var temperatureTwo = response.list[8].main.temp
+        $(".temperatureTwo").text("Temp: " + temperatureTwo +"°F");
+        var temperatureThree = response.list[16].main.temp
+        $(".temperatureThree").text("Temp: " + temperatureThree +"°F");
+        var temperatureFour = response.list[24].main.temp
+        $(".temperatureFour").text("Temp: " + temperatureFour +"°F");
+        var temperatureFive = response.list[32].main.temp
+        $(".temperatureFive").text("Temp: " + temperatureFive +"°F");
+
+        var humidityOne = response.list[0].main.humidity
+        $(".humidityOne").text("Humidity: " + humidityOne +"%");
+        var humidityTwo = response.list[8].main.humidity
+        $(".humidityTwo").text("Humidity: " + humidityTwo +"%");
+        var humidityThree = response.list[16].main.humidity
+        $(".humidityThree").text("Humidity: " + humidityThree +"%");
+        var humidityFour = response.list[24].main.humidity
+        $(".humidityFour").text("Humidity: " + humidityFour +"%");
+        var humidityFive = response.list[32].main.humidity
+        $(".humidityFive").text("Humidity: " + humidityFive +"%");
+    })
+}
