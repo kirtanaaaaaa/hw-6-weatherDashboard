1bf11ef6d42baabc99405c5a2db80be4fa37cb19
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-06-28 21:56:40 -0400

    current day weather

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-var date = moment().format("MMMM Do YYYY");
-$(".date").text(date);
+var searchBtn = $("#searchBtn");
 
+searchBtn.click(currentWeather);
+function currentWeather() {
+  var city = $("#city").val();
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=61f00a845310c4a4251fd6bfc588f89b"
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var date = moment().format("MMMM Do, YYYY");
+    $(".date").text(date);
+    var cityName = response.name;
+    $(".city").text(cityName) 
+    var iconCode = response.weather[0].icon
+    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    $(".icon").attr("src", iconURL);
+    var temperature = (((response.main.temp - 273.15)*9)/5) + 32;
+    $(".temperature").text("Temperature: " + temperature + "°F");
+    var humidity = response.main.humidity;
+    $(".humidity").text("Humidity: " + humidity + "%");
+    var windSpeed = response.wind.speed;
+    $(".windSpeed").text("Wind Speed: " + windSpeed + "MPH");
+  })
+};
